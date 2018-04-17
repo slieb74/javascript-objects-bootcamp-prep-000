8101410dcfcecd93524a199ed9464eb06265a2d2
Sam <sammyliebman@gmail.com>
2018-04-17 00:30:51 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {};
+var playlist = {
+  artist: 'kanye west',
+  song: 'gorgeous'
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  
+}
+
+function removeFromPlaylist(playlist, artistName){
+  
+}
